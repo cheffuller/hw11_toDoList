a965cdb76cb14761963c7abd08e0f8908c04a452
cheffuller <cheffuller@gmail.com>
2023-05-30 21:35:15 -0500

    cleaned up formatting

--- script.js
+++ script.js
@@ -5,23 +5,22 @@ const fetchTodos = () => {
 const logFetchTodos = () => console.log(fetchTodos());
 
 const logTodos = () => {
-    fetchTodos()
-        .then(res => res.json())
-        .then(json => console.log(json))
-}
+  fetchTodos()
+    .then((res) => res.json())
+    .then((json) => console.log(json));
+};
 
 const populateTodos = () => {
-
-    fetchTodos()
-        .then(res => res.json())
-        .then(todos => {
-            todos.forEach((currentElement, index) => {
-                const title = todos[index]["title"]
-                const newTitle = document.createTextNode(title);
-                const list = document.getElementById("todo-list");
-                const newListItem = document.createElement("li");
-                newListItem.appendChild(newTitle)
-                document.body.insertBefore(newListItem, list)
-            });
-        })
-}
+  fetchTodos()
+    .then((res) => res.json())
+    .then((todos) => {
+      todos.forEach((currentElement, index) => {
+        const title = todos[index]["title"];
+        const newTitle = document.createTextNode(title);
+        const list = document.getElementById("todo-list");
+        const newListItem = document.createElement("li");
+        newListItem.appendChild(newTitle);
+        document.body.insertBefore(newListItem, list);
+      });
+    });
+};
