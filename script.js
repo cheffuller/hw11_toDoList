18048cb12bcf70eaf1c65cb66fedb74a85f37d09
cheffuller <cheffuller@gmail.com>
2023-05-30 21:34:11 -0500

    edits to javascript to create functionality

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
-    // We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
+const fetchTodos = () => {
+  return fetch("https://jsonplaceholder.typicode.com/todos");
+};
 
-    let arrayOfTodos = [
-        {
-        "userId": 14,
-        "id": 1,
-        "title": "delectus aut autem",
-        "completed": false
-    },
-    {
-        "userId": 20,
-        "id": 2,
-        "title": "delectus aut autem",
-        "completed": false
-    }]
-    
-    const fetchTodos = () => {
-        fetch('https://jsonplaceholder.typicode.com/todos')
-        .then( (response) => response.json())
-        .then( (json) => arrayOfTodos = json)
-    }
-    
-    const logTodos = () => {
-        console.log(arrayOfTodos)
-    }
-    
-    const populateTodos = () => {
-    
-    }
+const logFetchTodos = () => console.log(fetchTodos());
+
+const logTodos = () => {
+    fetchTodos()
+        .then(res => res.json())
+        .then(json => console.log(json))
+}
+
+const populateTodos = () => {
+
+    fetchTodos()
+        .then(res => res.json())
+        .then(todos => {
+            todos.forEach((currentElement, index) => {
+                const title = todos[index]["title"]
+                const newTitle = document.createTextNode(title);
+                const list = document.getElementById("todo-list");
+                const newListItem = document.createElement("li");
+                newListItem.appendChild(newTitle)
+                document.body.insertBefore(newListItem, list)
+            });
+        })
+}
